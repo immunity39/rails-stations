0926ffa08f32f34d43679d1c62311b3cde3065fe
immunity39 <so_kaicyo@icloud.com>
2023-08-08 04:55:57 +0900

    rails-station01

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+movies = Movie.create([{name: 'Star Wars', year: 1999, description: "text", image_url: 'https://picsum.photos/200/300', is_showing: false}])
