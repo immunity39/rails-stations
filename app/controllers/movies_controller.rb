032402db5c3fbc8d5d172e4aba9c9796b9bfc980
immunity39 <so_kaicyo@icloud.com>
2023-08-10 09:26:46 +0900

    rails-stations06

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,14 @@
 class MoviesController < ApplicationController
   def index
     @movie = Movie.all
+
+    if params[:keyword].present?
+      @movie = @movie.search_by_word(params[:keyword])
+    end
+
+    if params[:is_showing].present?
+      @movie = params[:is_showing] == "1" ? @movie.showing : @movie.upcoming
+    end
+
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,8 @@ class Movie < ApplicationRecord
   validates :description, presence: true
   validates :image_url, presence: true
 
+  scope :showing, -> { where(is_showing: true)}
+  scope :upcoming, -> { where(is_showing: false)}
+  scope :search_by_word, -> (keyword) { where("name LIKE ? OR description LIKE ?", "%#{keyword}%", "%#{keyword}%") if keyword.present? }
+
 end
